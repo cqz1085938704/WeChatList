165ed9ddd35518eda8f7764799169ab2555a8c78
caiyao's Mac <caiyaolq@163.com>
2015-10-07 14:27:15 +0800

    Initial Commit

---
+++ WeChatFriendsList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChatFriendsList
+//
+//  Created by caiyao's Mac on 15/10/7.
+//  Copyright © 2015年 core's Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChatFriendsList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatFriendsList
+//
+//  Created by caiyao's Mac on 15/10/7.
+//  Copyright © 2015年 core's Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
